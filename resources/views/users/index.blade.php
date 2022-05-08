953450b7f1174d88aebda4632ed121b77a05928c
Banka_Rafal <rbanka221@gmail.com>
2022-05-08 19:16:32 +0200

    dodanie tabeli uzytkownikow

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -6,30 +6,22 @@
   <thead class="thead-dark">
     <tr>
       <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
+      <th scope="col">Imiê</th>
+      <th scope="col">Nazwisko</th>
+      <th scope="col">Numer telefonu</th>
+      <th scope="col">Email</th>
     </tr>
   </thead>
   <tbody>
+  @foreach($users as $user)
     <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
+      <th scope="row">{{$user->id}}</th>
+      <td>{{$user->name}}</td>
+      <td>{{$user->surname}}</td>
+      <td>{{$user->phone_number}}</td>
+      <td>{{$user->email}}</td>
     </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td>Larry</td>
-      <td>the Bird</td>
-      <td>@twitter</td>
-    </tr>
+   @endforeach
   </tbody>
 </table>
 </div>
