df849780df3fd3ade8c26e41acebd9cd9e0200a6
kfyda <kfyda>
2022-05-08 13:47:37 +0200

    model - przesylka

---
+++ app/Http/Controllers/PrzesylkaController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Przesylka;
+use Illuminate\Http\Request;
+
+class PrzesylkaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Przesylka  $przesylka
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Przesylka $przesylka)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Przesylka  $przesylka
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Przesylka $przesylka)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Przesylka  $przesylka
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Przesylka $przesylka)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Przesylka  $przesylka
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Przesylka $przesylka)
+    {
+        //
+    }
+}
