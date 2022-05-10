ad11e3c19b74e2d4724a7e0cac05ea13392c5a22
Banka_Rafal <rbanka221@gmail.com>
2022-05-10 19:17:42 +0200

    logowanie admin i uzytkownik v2

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,7 +19,10 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'surname',
+        'phone_number',
         'email',
+        'role',
         'password',
     ];
 
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\KurierController;
 use App\Http\Controllers\KlientController;
 use App\Http\Controllers\MagazynierController;
+use App\Http\Controllers\UserController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -21,10 +23,10 @@ Route::get('/', function () {
 });
 
 Route::get('hello', [HelloWorldController::class, 'show']);
+Route::get('/users/list', [UserController::class, 'index'])->middleware('can:isAdmin');
 Route::get('kuriers',[KurierController::class,'getData']);
 Route::get('klients',[KlientController::class,'getData']);
 Route::get('magazyniers',[MagazynierController::class,'getData']);
-
 
 Auth::routes();
 
