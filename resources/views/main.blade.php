1bd15695636ffd9b32da3f6c28e11ccdbc95d45e
kfyda <kfyda>
2022-05-21 20:39:16 +0200

    Strona główna - finalnie

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -35,38 +35,7 @@
                 <h1 class="m-0 display-7 text-primary">Firma kurierska</h1>
             </a>
         </div>
-        <div class="col-lg-9">
-            <div class="row bg-dark d-none d-lg-flex">
-                <div class="col-lg-7 text-left text-white">
-                    <div class="h-100 d-inline-flex align-items-center border-right border-primary py-2 px-3">
-                        <i class="fa fa-envelope text-primary mr-2"></i>
-                        <small>firma_kurierska@projekt.com</small>
-                    </div>
-                    <div class="h-100 d-inline-flex align-items-center py-2 px-2">
-                        <i class="fa fa-phone-alt text-primary mr-2"></i>
-                        <small>+123 456 789</small>
-                    </div>
-                </div>
-                <div class="col-lg-5 text-right">
-                    <div class="d-inline-flex align-items-center pr-2">
-                        <a class="text-primary p-2" href="">
-                            <i class="fab fa-facebook-f"></i>
-                        </a>
-                        <a class="text-primary p-2" href="">
-                            <i class="fab fa-twitter"></i>
-                        </a>
-                        <a class="text-primary p-2" href="">
-                            <i class="fab fa-linkedin-in"></i>
-                        </a>
-                        <a class="text-primary p-2" href="">
-                            <i class="fab fa-instagram"></i>
-                        </a>
-                        <a class="text-primary p-2" href="">
-                            <i class="fab fa-youtube"></i>
-                        </a>
-                    </div>
-                </div>
-            </div>
+
             <nav class="navbar navbar-expand-lg bg-white navbar-light p-0">
                 <a href="" class="navbar-brand d-block d-lg-none">
                     <h1 class="m-0 display-4 text-primary">Firma kurierska</h1>
@@ -89,7 +58,7 @@
                             </div>
                         </div>
                     </div>
-                    <a href="" class="btn btn-primary mr-3 d-none d-lg-block">Get A Quote</a>
+                    <a href="" class="btn btn-primary mr-3 d-none d-lg-block">Sprawdź</a>
                 </div>
             </nav>
         </div>
@@ -182,18 +151,18 @@
     <div class="row pt-5">
         <div class="col-lg-3 col-md-6 mb-5">
             <a href="/main" class="navbar-brand">
-                <h1 class="m-0 mt-n3 display-4 text-primary">Klean</h1>
+                <h1 class="m-0 mt-n3 display-7 text-primary">Firma kurierska</h1>
             </a>
-            <p>Volup amet magna clita tempor. Tempor sea eos vero ipsum. Lorem lorem sit sed elitr sed kasd et</p>
-            <h5 class="font-weight-semi-bold text-white mb-2">Opening Hours:</h5>
-            <p class="mb-1">Mon – Sat, 8AM – 5PM</p>
-            <p class="mb-0">Sunday: Closed</p>
+            <p>Oferujemy najlepsze usługi kurierskie w całym województwie</p>
+            <h5 class="font-weight-semi-bold text-white mb-2">Godziny otwarcia:</h5>
+            <p class="mb-1">Poniedziałek – Sobota: 8 – 16</p>
+            <p class="mb-0">Niedziela: Zamknięte</p>
         </div>
         <div class="col-lg-3 col-md-6 mb-5">
-            <h4 class="font-weight-semi-bold text-primary mb-4">Get In Touch</h4>
-            <p><i class="fa fa-map-marker-alt text-primary mr-2"></i>123 Street, New York, USA</p>
-            <p><i class="fa fa-phone-alt text-primary mr-2"></i>+012 345 67890</p>
-            <p><i class="fa fa-envelope text-primary mr-2"></i>info@example.com</p>
+            <h4 class="font-weight-semi-bold text-primary mb-4">Kontakt</h4>
+            <p><i class="fa fa-map-marker-alt text-primary mr-2"></i>123 Zamenhofa, Nowy Sącz, Polska</p>
+            <p><i class="fa fa-phone-alt text-primary mr-2"></i>+123 456 789</p>
+            <p><i class="fa fa-envelope text-primary mr-2"></i>firma_kurierska@projekt.com</p>
             <div class="d-flex justify-content-start mt-4">
                 <a class="btn btn-light btn-social mr-2" href="#"><i class="fab fa-twitter"></i></a>
                 <a class="btn btn-light btn-social mr-2" href="#"><i class="fab fa-facebook-f"></i></a>
@@ -202,48 +171,38 @@
             </div>
         </div>
         <div class="col-lg-3 col-md-6 mb-5">
-            <h4 class="font-weight-semi-bold text-primary mb-4">Quick Links</h4>
+            <h4 class="font-weight-semi-bold text-primary mb-4">Skróty</h4>
             <div class="d-flex flex-column justify-content-start">
-                <a class="text-white mb-2" href="#"><i class="fa fa-angle-right mr-2"></i>Home</a>
-                <a class="text-white mb-2" href="#"><i class="fa fa-angle-right mr-2"></i>About Us</a>
-                <a class="text-white mb-2" href="#"><i class="fa fa-angle-right mr-2"></i>Our Services</a>
-                <a class="text-white mb-2" href="#"><i class="fa fa-angle-right mr-2"></i>Our Projects</a>
-                <a class="text-white" href="#"><i class="fa fa-angle-right mr-2"></i>Contact Us</a>
+                <a class="text-white mb-2" href="#"><i class="fa fa-angle-right mr-2"></i>Strona główna</a>
+                <a class="text-white mb-2" href="#"><i class="fa fa-angle-right mr-2"></i>O nas</a>
+                <a class="text-white mb-2" href="#"><i class="fa fa-angle-right mr-2"></i>Nasz serwis</a>
+                <a class="text-white mb-2" href="#"><i class="fa fa-angle-right mr-2"></i>Nasz projekt</a>
+                <a class="text-white" href="#"><i class="fa fa-angle-right mr-2"></i>Kontakt</a>
             </div>
         </div>
         <div class="col-lg-3 col-md-6 mb-5">
-            <h4 class="font-weight-semi-bold text-primary mb-4">Newsletter</h4>
-            <p>Rebum labore lorem dolores kasd est, et ipsum amet et at kasd, ipsum sea tempor magna tempor. Accu kasd sed ea duo ipsum.</p>
-            <div class="w-100">
-                <div class="input-group">
-                    <input type="text" class="form-control border-0" style="padding: 25px;" placeholder="Your Email">
-                    <div class="input-group-append">
-                        <button class="btn btn-primary px-4">Sign Up</button>
-                    </div>
-                </div>
-            </div>
+            <img class="img-fluid" style="width: 80%;" src="{{asset('img/box.png')}}">
         </div>
-    </div>
 </div>
 <div class="container-fluid bg-dark text-white border-top py-4 px-sm-3 px-md-5" style="border-color: #3E3E4E !important;">
     <div class="row">
         <div class="col-lg-6 text-center text-md-left mb-3 mb-md-0">
-            <p class="m-0 text-white">&copy; <a href="#">Your Site Name</a>. All Rights Reserved. Designed by <a href="https://htmlcodex.com">HTML Codex</a>
+            <p class="m-0 text-white">&copy; <a href="#">Copyright</a>.2022 <a href="https://htmlcodex.com"></a>
             </p>
         </div>
         <div class="col-lg-6 text-center text-md-right">
             <ul class="nav d-inline-flex">
                 <li class="nav-item">
-                    <a class="nav-link text-white py-0" href="#">Privacy</a>
+                    <a class="nav-link text-white py-0" href="#">Prywatność</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-white py-0" href="#">Terms</a>
+                    <a class="nav-link text-white py-0" href="#">Warunki</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-white py-0" href="#">FAQs</a>
+                    <a class="nav-link text-white py-0" href="#">Forum</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-white py-0" href="#">Help</a>
+                    <a class="nav-link text-white py-0" href="#">Pomoc</a>
                 </li>
             </ul>
         </div>
