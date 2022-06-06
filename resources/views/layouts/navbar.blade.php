d3726bbc5e3fd04fc4c9fc4b0fec19c6472e9516
Fyda_Kamil <kf8957@student.pwsz-ns.edu.pl>
2022-06-06 21:01:52 +0200

    Panel admina - poprawki

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -57,10 +57,10 @@
                         @endif
                     @else
                             @can('isAdmin')
-                                <a href="{{ route('klient.hello') }}" class="nav-item nav-link active">Strona główna - administrator</a>
-                                <a href="{{ route('Przesylkas.indexklient') }}" class="nav-item nav-link">Kontroluj przesyłki</a>
-                                <a href="{{ route('Przesylkas.indexklient') }}" class="nav-item nav-link">Dodaj paczkomat</a>
-                                <a href="/contact" class="nav-item nav-link">Powiadomienia</a>
+                                <a href="{{ route('adminmain') }}" class="nav-item nav-link active">Strona główna - administrator</a>
+                                <a href="{{ route('Przesylkas.indexklient') }}" class="nav-item nav-link">Lista pracowników</a>
+                                <a href="{{ route('paczkomat.create') }}" class="nav-item nav-link">Dodaj paczkomat</a>
+                                <a href="/#" class="nav-item nav-link">Powiadomienia</a>
                             @endcan
                         @can('isUser')
                             <a href="{{ route('klient.hello') }}" class="nav-item nav-link active">Strona główna - klient</a>
