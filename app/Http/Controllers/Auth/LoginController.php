9a6375fb3c659e99e7b5f0025dc92211f47129df
Banka_Rafal <rbanka221@gmail.com>
2022-06-06 14:12:36 +0200

    Strona główna - przenoszenie dla poszczególnych użytkowników po logowaniu

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -27,6 +28,21 @@ class LoginController extends Controller
      * @var string
      */
     protected $redirectTo = RouteServiceProvider::HOME;
+    protected function redirectTo()
+    {
+        if (Auth::user()->role == 'admin')
+        {
+            return route('home');
+        }
+        elseif (Auth::user()->role == 'user')
+        {
+            return route('klient.hello');
+        }
+        elseif (Auth::user()->role == 'deliveryman')
+        {
+            return route('deliveryman.hello');
+        }
+    }
 
     /**
      * Create a new controller instance.
